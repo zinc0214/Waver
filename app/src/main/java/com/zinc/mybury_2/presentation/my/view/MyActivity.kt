4797e75a3ba539465331c63951d2cbaa02426b1a
zinc <zinc0214@gmail.com>
2021-08-22 21:20:11 +0900

    [My] 1차적으로 tabLayout 개발

--- app/src/main/java/com/zinc/mybury_2/presentation/my/view/MyActivity.kt
+++ app/src/main/java/com/zinc/mybury_2/presentation/my/view/MyActivity.kt
@@ -1,14 +1,19 @@
 package com.zinc.mybury_2.presentation.my.view
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayout
 import com.zinc.data.models.MyProfileInfo
 import com.zinc.data.models.MyState
 import com.zinc.domain.models.BadgeType
 import com.zinc.mybury_2.R
 import com.zinc.mybury_2.compose.ui.component.ProfileCircularProgressBarWidget
 import com.zinc.mybury_2.databinding.ActivityMyBinding
+
 
 class MyActivity : AppCompatActivity() {
 
@@ -32,6 +37,49 @@ class MyActivity : AppCompatActivity() {
             )
             val imageView = ProfileCircularProgressBarWidget("www.naver.com", 0.5f, this@MyActivity)
             profileImageLayout.addView(imageView)
+
+            wrapTabIndicatorToTitle(tabLayout, 3, 3)
         }
+    }
+
+    fun wrapTabIndicatorToTitle(tabLayout: TabLayout, externalMargin: Int, internalMargin: Int) {
+        val tabStrip: View = tabLayout.getChildAt(0)
+        if (tabStrip is ViewGroup) {
+            val tabStripGroup = tabStrip as ViewGroup
+            val childCount = (tabStrip as ViewGroup).childCount
+            for (i in 0 until childCount) {
+                val tabView: View = tabStripGroup.getChildAt(i)
+                //set minimum width to 0 for instead for small texts, indicator is not wrapped as expected
+                tabView.minimumWidth = 0
+                // set padding to 0 for wrapping indicator as title
+                tabView.setPadding(0, tabView.paddingTop, 0, tabView.paddingBottom)
+                // setting custom margin between tabs
+                if (tabView.layoutParams is MarginLayoutParams) {
+                    val layoutParams = tabView.layoutParams as MarginLayoutParams
+                    when (i) {
+                        0 -> {
+                            // left
+                            settingMargin(layoutParams, externalMargin, internalMargin)
+                        }
+                        childCount - 1 -> {
+                            // right
+                            settingMargin(layoutParams, internalMargin, externalMargin)
+                        }
+                        else -> {
+                            // internal
+                            settingMargin(layoutParams, internalMargin, internalMargin)
+                        }
+                    }
+                }
+            }
+            tabLayout.requestLayout()
+        }
+    }
+
+    private fun settingMargin(layoutParams: MarginLayoutParams, start: Int, end: Int) {
+        layoutParams.marginStart = start
+        layoutParams.marginEnd = end
+        layoutParams.leftMargin = start
+        layoutParams.rightMargin = end
     }
 }
