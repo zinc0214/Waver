8cf665f183a9587ac0a60e3c73d9109503fb95d2
zinc <zinc0214@gmail.com>
2021-09-20 23:24:00 +0900

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val composeVersion by extra("1.0.0-beta09")
-    val hiltVersion by extra("2.37")
+    val hiltVersion by extra("2.38.1")
     val androidXHiltVersion by extra("1.0.0")
     val coroutinesVersion by extra("1.5.0")
     val retrofitVersion by extra("2.9.0")
@@ -14,7 +14,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta02")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha12")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.21")
