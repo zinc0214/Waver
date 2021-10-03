23520e48fbf84ede84c048038d33158884adf658
zinc <zinc0214@gmail.com>
2021-10-04 02:24:37 +0900

    [Home] MyActivity -> MyFragment 로 전환 및 fragment 유지

---
+++ app/src/main/java/com/zinc/mybury_2/presentation/my/view/fragment/MyFragment.kt
@@ -0,0 +1,97 @@
+package com.zinc.mybury_2.presentation.my.view.fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.zinc.data.models.BadgeType
+import com.zinc.domain.models.TopProfile
+import com.zinc.mybury_2.R
+import com.zinc.mybury_2.compose.ui.component.ProfileCircularProgressBarWidget
+import com.zinc.mybury_2.databinding.FragmentMyBinding
+import com.zinc.mybury_2.ui.MyTabCustom
+
+class MyFragment : Fragment() {
+    private lateinit var binding: FragmentMyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_my, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setUpViews()
+        setUpTabLayout()
+    }
+
+    private fun setUpViews() {
+        binding.apply {
+            profileInfo = TopProfile(
+                nickName = "Hana",
+                profileImg = "ddd",
+                badgeType = BadgeType.TRIP1,
+                titlePosition = "안녕 반가우이",
+                bio = "나는 ESFP 한아라고 불러줘?",
+                followerCount = "10",
+                followingCount = "15"
+            )
+            val imageView =
+                ProfileCircularProgressBarWidget("www.naver.com", 0.5f, requireContext())
+            profileImageLayout.addView(imageView)
+        }
+    }
+
+    private fun setUpTabLayout() {
+
+        val myTabView = MyTabCustom(requireContext())
+        myTabView.setUpTabDesigns(binding.myTabLayout)
+
+        val allFragment = AllBucketListFragment.newInstance()
+        val categoryFragment = CategoryListFragment.newInstance()
+        val ddayFragment = DdayBucketListFragment.newInstance()
+
+        childFragmentManager.beginTransaction().add(R.id.myFrameLayout, allFragment).commit()
+
+        binding.myTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                Log.e("ayhan", "tabPosition : ${tab.position}")
+                val currentFragment = when (tab.position) {
+                    0 -> {
+                        allFragment
+                    }
+                    1 -> {
+                        categoryFragment
+                    }
+                    else -> {
+                        ddayFragment
+                    }
+                }
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.myFrameLayout, currentFragment).commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+
+        })
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MyFragment()
+    }
+}
