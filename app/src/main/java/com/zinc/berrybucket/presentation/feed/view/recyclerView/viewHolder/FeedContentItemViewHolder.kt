4f24ddd38c238443c99367d0186feb950b9ce669
zinc <zinc0214@gmail.com>
2021-10-24 17:04:14 +0900

    [Feed] ImageViewPager 와 TabLayout 연결

--- app/src/main/java/com/zinc/berrybucket/presentation/feed/view/recyclerView/viewHolder/FeedContentItemViewHolder.kt
+++ app/src/main/java/com/zinc/berrybucket/presentation/feed/view/recyclerView/viewHolder/FeedContentItemViewHolder.kt
@@ -1,14 +1,17 @@
 package com.zinc.berrybucket.presentation.feed.view.recyclerView.viewHolder
 
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.material.tabs.TabLayoutMediator
 import com.zinc.berrybucket.R
 import com.zinc.berrybucket.databinding.WidgetFeedContentBinding
 import com.zinc.berrybucket.model.FeedInfo
 import com.zinc.berrybucket.presentation.feed.view.recyclerView.adapter.FeedImageViewPagerAdapter
 import com.zinc.berrybucket.util.dp2px
+
 
 class FeedContentItemViewHolder(private val binding: WidgetFeedContentBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -23,8 +26,12 @@ class FeedContentItemViewHolder(private val binding: WidgetFeedContentBinding) :
 
             info.imageList?.let {
                 imageViewPager.adapter = FeedImageViewPagerAdapter(it)
+                TabLayoutMediator(
+                    tabLayout, imageViewPager
+                ) { tab, position ->
+                    Toast.makeText(context, "Tab $position", Toast.LENGTH_SHORT).show()
+                }.attach()
             }
-
         }
     }
 }
