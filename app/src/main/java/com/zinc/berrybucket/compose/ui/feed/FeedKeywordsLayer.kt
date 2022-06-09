136f396d4a3ade89b996c45b66c64528b4dfc206
hana <zinc0214@gmail.com>
2022-06-10 00:14:33 +0900

    [홈 > 피드] 키워드 선택 화면 스크롤 이벤트 수정

--- app/src/main/java/com/zinc/berrybucket/compose/ui/feed/FeedKeywordsLayer.kt
+++ app/src/main/java/com/zinc/berrybucket/compose/ui/feed/FeedKeywordsLayer.kt
@@ -1,5 +1,6 @@
 package com.zinc.berrybucket.compose.ui.feed
 
+import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -22,6 +23,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.zinc.berrybucket.R
 import com.zinc.berrybucket.compose.theme.*
 import com.zinc.berrybucket.compose.ui.common.RoundChip
@@ -36,48 +39,82 @@ fun FeedKeywordsLayer(keywords: List<FeedKeyWord>, recommendClicked: () -> Unit)
         modifier = Modifier
             .fillMaxHeight()
             .background(Gray2)
-            .padding(horizontal = 28.dp)
 
     ) {
         val scrollState = rememberLazyGridState()
-        val scrollOffset: Float = min(
-            1f,
-            1 - (scrollState.firstVisibleItemScrollOffset / 600f + scrollState.firstVisibleItemIndex)
+        val isScrolled = scrollState.firstVisibleItemIndex != 0
+        Log.e(
+            "ayhan",
+            "firstVisibleItemScrollOffset ; ${scrollState.firstVisibleItemScrollOffset}, ${scrollState.firstVisibleItemIndex}"
         )
-        Column {
+
+        val scrollOffset: Float = min(
+            1f, 1 - (scrollState.firstVisibleItemScrollOffset / 150f)
+        )
+        Log.e("ayhan", "scrollOffset : $scrollOffset")
+
+
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (toolbar, divider, body) = createRefs()
+
             FeedCollapsingToolbar(
-                scrollOffset = scrollOffset
+                scrollOffset = scrollOffset,
+                isScrolled = isScrolled,
+                modifier = Modifier
+                    .constrainAs(toolbar) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .padding(horizontal = 28.dp)
             )
-            if (scrollOffset <= 0.0) {
-                Divider(
-                    color = Gray3
-                )
+
+            if (scrollOffset <= 0.0 || isScrolled) {
+                Divider(color = Gray3, modifier = Modifier.constrainAs(divider) {
+                    top.linkTo(toolbar.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
             }
-            BodyContent(
-                state = scrollState,
+
+            BodyContent(state = scrollState,
                 keywords = keywords,
-                recommendClicked = recommendClicked
+                recommendClicked = recommendClicked,
+                modifier = Modifier
+                    .constrainAs(body) {
+                        top.linkTo(toolbar.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                    }
+                    .padding(horizontal = 28.dp)
             )
         }
     }
-
 }
 
 @Composable
-private fun FeedCollapsingToolbar(scrollOffset: Float) {
+private fun FeedCollapsingToolbar(
+    scrollOffset: Float, isScrolled: Boolean, modifier: Modifier
+) {
     val textSize by animateDpAsState(targetValue = max(16.dp, 24.dp * scrollOffset))
     val topPadding by animateDpAsState(targetValue = max(34.dp, 40.dp * scrollOffset))
     val bottomPadding by animateDpAsState(targetValue = max(14.dp, 40.dp * scrollOffset))
 
     Text(
-        text = if (scrollOffset > 0.0) stringResource(id = R.string.feedRecommendTitle) else stringResource(
+        text = if (scrollOffset > 0.0 && !isScrolled) stringResource(id = R.string.feedRecommendTitle) else stringResource(
             id = R.string.feedRecommendSmallTitle
         ),
-        fontSize = textSize.value.sp,
-        textAlign = if (scrollOffset > 0.0) TextAlign.Start else TextAlign.Center,
-        modifier = Modifier
+        fontSize = if (isScrolled) 16.sp else textSize.value.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = if (scrollOffset > 0.0 && !isScrolled) TextAlign.Start else TextAlign.Center,
+        modifier = modifier
             .fillMaxWidth()
-            .padding(top = topPadding, bottom = bottomPadding)
+            .padding(
+                top = if (isScrolled) 34.dp else topPadding,
+                bottom = if (isScrolled) 14.dp else bottomPadding
+            )
     )
 
 }
@@ -86,36 +123,28 @@ private fun FeedCollapsingToolbar(scrollOffset: Float) {
 private fun BodyContent(
     state: LazyGridState,
     keywords: List<FeedKeyWord>,
-    recommendClicked: () -> Unit
+    recommendClicked: () -> Unit,
+    modifier: Modifier
 ) {
-    Box(
-        content = {
-            ChipBodyContent(
-                modifier = Modifier.padding(bottom = 100.dp),
-                state = state,
-                keywords = keywords
-            )
-            BucketRecommendButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 28.dp),
-                recommendClicked
-            )
-        }
-    )
+    Box(modifier = modifier, content = {
+        ChipBodyContent(
+            modifier = Modifier.padding(bottom = 100.dp), state = state, keywords = keywords
+        )
+        BucketRecommendButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 28.dp), recommendClicked
+        )
+    })
 }
 
 @Composable
 private fun ChipBodyContent(
-    modifier: Modifier = Modifier,
-    state: LazyGridState,
-    keywords: List<FeedKeyWord>
+    modifier: Modifier = Modifier, state: LazyGridState, keywords: List<FeedKeyWord>
 ) {
     androidx.compose.foundation.lazy.grid.LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 90.dp),
-        state = state,
-        modifier = modifier
+        columns = GridCells.Adaptive(minSize = 90.dp), state = state, modifier = modifier
     ) {
         items(items = keywords) { keywordItem ->
             var selected by remember { mutableStateOf(false) }
@@ -124,12 +153,9 @@ private fun ChipBodyContent(
                     .padding(horizontal = 8.5.dp, vertical = 14.dp)
                     .padding(bottom = if (keywords.last() == keywordItem) 69.dp else 0.dp)
                     .defaultMinSize(minWidth = 90.dp, minHeight = 48.dp)
-                    .selectable(
-                        selected = selected,
-                        onClick = {
-                            selected = !selected
-                        }
-                    ),
+                    .selectable(selected = selected, onClick = {
+                        selected = !selected
+                    }),
                 chipRadius = 24.dp,
                 textModifier = Modifier.padding(horizontal = 8.dp, vertical = 14.dp),
                 selectedColor = Main3,
@@ -143,14 +169,12 @@ private fun ChipBodyContent(
 
 @Composable
 private fun BucketRecommendButton(modifier: Modifier = Modifier, recommendClicked: () -> Unit) {
-    Box(
-        modifier = modifier
-            .clip(RoundedCornerShape(12.dp))
-            .background(Main4)
-            .clickable {
-                recommendClicked()
-            }
-    ) {
+    Box(modifier = modifier
+        .clip(RoundedCornerShape(12.dp))
+        .background(Main4)
+        .clickable {
+            recommendClicked()
+        }) {
         Text(
             text = stringResource(id = R.string.recommendBucketList),
             textAlign = TextAlign.Center,
