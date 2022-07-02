e775733cd7c9257167246ec1b4d58ccd015dca76
hana <zinc0214@gmail.com>
2022-07-02 16:52:49 +0900

    불필요한 코드 제거, 공백 제거

--- app/src/main/java/com/zinc/berrybucket/ui/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/zinc/berrybucket/ui/presentation/login/LoginActivity.kt
@@ -4,10 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
-import com.zinc.berrybucket.ui.compose.theme.BaseTheme
 import com.zinc.berrybucket.ui.presentation.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,9 +14,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // This app draws behind the system bars, so we want to handle fitting system windows
-        // WindowCompat.setDecorFitsSystemWindows(window, false)
 
         viewModel.accessToken.observe(this) {
             if (it.isNullOrEmpty()) {
@@ -33,19 +26,6 @@ class LoginActivity : AppCompatActivity() {
 
         viewModel.joinResponse.observe(this) {
             viewModel.loadToken()
-        }
-
-
-    }
-}
-
-@Composable
-private fun LoginView() {
-    BaseTheme {
-        TextButton(onClick = {
-
-        }) {
-            Text(text = "시작하기")
         }
     }
 }
