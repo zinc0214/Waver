408b2f0a610437e98058ba1a5680342b60c0d349
zinc0214 <zinc0214@gmail.com>
2023-10-01 02:47:59 +0900

    target, compile version update

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -1,11 +1,11 @@
 @file:Suppress("ClassName")
 
 object Versions {
-    const val compileSdk = 33
-    const val buildTools = "33.0.1"
+    const val compileSdk = 34
+    const val buildTools = "34.0.0"
 
     const val minSdk = 26
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val versionCode = 1
     const val versionName = "1.0.0"
 }
