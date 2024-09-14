50b542ad03cd9364646058a1044f029c5358b1ee
zinc0214 <zinc0214@gmail.com>
2024-09-14 19:32:59 +0900

    api path 수정

--- data/src/main/java/com/zinc/data/di/NetworkModule.kt
+++ data/src/main/java/com/zinc/data/di/NetworkModule.kt
@@ -36,7 +36,7 @@ object NetworkModule {
     fun provideRetrofit(
         okHttpClient: OkHttpClient
     ): Retrofit = Retrofit.Builder()
-        .baseUrl("http://3.35.131.48/")
+        .baseUrl("http://100.24.67.193:8080/")
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient)
         .build()
