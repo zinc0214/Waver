3e8e54aa8a93d95e7289d9e03056acb4262383ad
zinc0214 <zinc0214@gmail.com>
2024-12-15 15:48:55 +0900

    version code 업데이트

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -6,6 +6,6 @@ object Versions {
 
     const val minSdk = 26
     const val targetSdk = 35
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
 }
