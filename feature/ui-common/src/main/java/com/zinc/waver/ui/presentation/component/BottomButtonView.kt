f478515f035d35613c6246e7c1c7b833e284caf6
zinc0214 <zinc0214@gmail.com>
2025-12-31 15:16:37 +0900

    [마이] 버킷리스트 필터 디자인 수정

--- feature/ui-common/src/main/java/com/zinc/waver/ui/presentation/component/BottomButtonView.kt
+++ feature/ui-common/src/main/java/com/zinc/waver/ui/presentation/component/BottomButtonView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -41,7 +42,7 @@ fun BottomButtonView(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(55.dp)
+            .wrapContentHeight()
             .background(color = Gray1)
     ) {
         Divider(
@@ -51,7 +52,9 @@ fun BottomButtonView(
                 .background(color = colorResource(id = R.color._dbdbdb))
         )
 
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)) {
             if (negativeEvent != null && negative != null) {
                 Box(
                     modifier = Modifier
@@ -96,10 +99,17 @@ fun BottomButtonView(
                 )
             }
         }
+
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(0.6.dp)
+                .background(color = colorResource(id = R.color._dbdbdb))
+        )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun BottomButtonPreview() {
     Column {
