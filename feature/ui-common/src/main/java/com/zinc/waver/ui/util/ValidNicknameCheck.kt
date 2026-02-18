ed6257af33be851279678e5504a03d3bdbcdd158
zinc0214 <zinc0214@gmail.com>
2026-02-18 20:13:47 +0900

    [프로필수정] 3글자 미만인 경우, 저장될 수 없도록 수정

--- feature/ui-common/src/main/java/com/zinc/waver/ui/util/ValidNicknameCheck.kt
+++ feature/ui-common/src/main/java/com/zinc/waver/ui/util/ValidNicknameCheck.kt
@@ -1,6 +1,7 @@
 package com.zinc.waver.ui.util
 
 fun isValidNicknameCheck(nickname: String): Boolean {
+    if (nickname.length < 3) return false
     val specialCharRegex = Regex("[^a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ]")
     val hasSpecialChar = nickname.any { specialCharRegex.matches(it.toString()) }
     return !hasSpecialChar
